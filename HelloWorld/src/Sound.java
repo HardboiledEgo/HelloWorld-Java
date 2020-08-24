bdc4787bbae2523323aff90a85738aa8dfe582c1
HardboiledEgo <51846859+HardboiledEgo@users.noreply.github.com>
2020-08-24 11:05:46 +0300

    Initial commit

---
+++ HelloWorld/src/Sound.java
@@ -0,0 +1,12 @@
+
+public class Sound {
+	public static void main(String args[]) {
+		double dist, t;
+		
+		t = 10;
+		dist = (330 * t)/2;
+		
+		System.out.println("Расстояние до объекта от которого отразилось эхо: " + dist + " метров.");
+	}
+
+}
